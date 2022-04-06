15c9f3ae45fde896adf855cb7453976d5d877b19
Aditya Menon <adit.gmenon96@gmail.com>
2022-04-07 00:53:45 +0530

    Add building project to workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Testing
+          Learn Reaction Videos
         </a>
       </header>
     </div>
