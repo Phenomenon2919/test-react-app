0bfe2e6e451005a2fb45fe2e3c39e26929b1f26d
Aditya Menon <adit.gmenon96@gmail.com>
2022-04-07 12:36:42 +0530

    feat: Adding Releases to Workflow

---
+++ release-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/Phenomenon2919/test-react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
