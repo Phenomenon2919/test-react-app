877037a189efa6b11d18ec59eb167a948d443f2f
Aditya Menon <adit.gmenon96@gmail.com>
2022-04-18 00:41:11 +0530

    feat: Added core comments, logs and export variables

--- .github/actions/greet/dist/index.js
+++ .github/actions/greet/dist/index.js
@@ -8473,14 +8473,22 @@ const github = __nccwpck_require__(5438);
 
 try{
     // throw( new Error("Error present!"));
+    core.debug("Sample Debug Message");
+    core.warning("Sample Warning Message");
+    core.error("Sample Error message");
+
     const name = core.getInput('who-to-greet');
+    core.setSecret(name);
     console.log(`Hello ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging Github actions');
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
 
+    core.exportVariable("CC", 'Jack');
 } catch(error){
 
     core.setFailed(error.message);
--- .github/actions/greet/index.js
+++ .github/actions/greet/index.js
@@ -3,14 +3,22 @@ const github = require("@actions/github");
 
 try{
     // throw( new Error("Error present!"));
+    core.debug("Sample Debug Message");
+    core.warning("Sample Warning Message");
+    core.error("Sample Error message");
+
     const name = core.getInput('who-to-greet');
+    core.setSecret(name);
     console.log(`Hello ${name}`);
 
     const time = new Date();
     core.setOutput("time", time.toTimeString());
 
+    core.startGroup('Logging Github actions');
     console.log(JSON.stringify(github, null, '\t'));
+    core.endGroup();
 
+    core.exportVariable("CC", 'Jack');
 } catch(error){
 
     core.setFailed(error.message);
